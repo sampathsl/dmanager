beef48a572b127413735c9d27bf70cb1c416f36e
sampath <sampath.thennakoon@gmail.com>
2019-02-01 06:45:12 +0530

    Add test cases

--- src/test/java/com/github/sampathsl/dmanager/service/DownloadTaskServiceImplTest.java
+++ src/test/java/com/github/sampathsl/dmanager/service/DownloadTaskServiceImplTest.java
@@ -4,13 +4,22 @@ import com.github.sampathsl.dmanager.model.DownloadTask;
 import com.github.sampathsl.dmanager.repository.DownloadTaskRepository;
 import com.github.sampathsl.dmanager.util.FileSizeStatus;
 import com.github.sampathsl.dmanager.util.FileSpeedStatus;
+import org.hamcrest.Matchers;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
 
 public class DownloadTaskServiceImplTest {
 
@@ -22,9 +31,24 @@ public class DownloadTaskServiceImplTest {
 
   private Optional<DownloadTask> downloadTaskOptional;
 
+  private List<DownloadTask> downloadTasks;
+
   @Before
   public void setUp() throws Exception {
     MockitoAnnotations.initMocks(this);
+    downloadTask =
+        new DownloadTask(
+            0l,
+            0l,
+            "http:/test.com/test.txt",
+            "/home/sampath",
+            "http",
+            LocalDateTime.now(),
+            LocalDateTime.now(),
+            0l,
+            0f,
+            FileSpeedStatus.UNKNOWN.toString(),
+            FileSizeStatus.UNKNOWN.toString());
     downloadTask =
         new DownloadTask(
             0l,
@@ -42,36 +66,44 @@ public class DownloadTaskServiceImplTest {
     ((DownloadTaskServiceImpl) downloadTaskService)
         .setDownloadTaskRepository(downloadTaskRepository);
     downloadTaskOptional = Optional.of(downloadTask);
+    downloadTasks = new ArrayList<DownloadTask>();
+    downloadTasks.add(downloadTask);
   }
 
   @Test
   public void testCreate() {
-    // TODO
+    Mockito.when(downloadTaskRepository.save(any(DownloadTask.class))).thenReturn(downloadTask);
+    Assert.assertThat(downloadTaskService.create(downloadTask), is(Matchers.notNullValue()));
   }
 
   @Test
   public void testFindById() {
-    // TODO
+    when(downloadTaskRepository.findById(1l)).thenReturn(downloadTaskOptional);
+    Assert.assertThat(downloadTaskService.findById(1l), is(Matchers.notNullValue()));
   }
 
   @Test
   public void testUpdate() {
-    // TODO
+    when(downloadTaskRepository.save(any(DownloadTask.class))).thenReturn(downloadTask);
+    Assert.assertThat(downloadTaskService.update(downloadTask), is(Matchers.notNullValue()));
   }
 
   @Test
   public void testDelete() {
-    // TODO
+    downloadTaskService.delete(downloadTask);
+    verify(downloadTaskRepository, times(1)).delete(eq(downloadTask));
   }
 
   @Test
   public void testCreateDownloadTasks() {
-    // TODO
+    when(downloadTaskRepository.save(any(DownloadTask.class))).thenReturn(downloadTask);
+    Assert.assertThat(
+        downloadTaskService.createDownloadTasks(downloadTasks), is(Matchers.notNullValue()));
   }
 
   @Test
   public void testFindBySessionId() {
-    // TODO
+    when(downloadTaskRepository.findAllDownloadTaskBySessionId(1l)).thenReturn(downloadTasks);
+    Assert.assertThat(downloadTaskService.findBySessionId(1l), is(Matchers.notNullValue()));
   }
-
 }
